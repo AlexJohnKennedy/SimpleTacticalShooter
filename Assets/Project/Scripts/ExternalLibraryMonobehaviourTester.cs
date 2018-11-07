2e294ad3b0f29501daf5042ae45286987b9cdbb0
AlexJohnKennedy <alexkfridges@gmail.com>
2018-11-08 03:21:12 +1100

    got external libraries to work in a trivial case, in this case compiling a .NET 4.6.1 external class library containing a single class, and used it in thihs unity project!

---
+++ Assets/Project/Scripts/ExternalLibraryMonobehaviourTester.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityExternalLibraryTEST1_NETv4_6_1;  // IMPORT TEST LIBRARIES, WHICH ARE IN THE PROJECT DIRECTORY AS DLL FILES. 
+
+public class ExternalLibraryMonobehaviourTester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // Simply try to print the string obtained by the external library
+        Debug.Log(TestLibraryFunctions.GetTestString());
+	}
+}
