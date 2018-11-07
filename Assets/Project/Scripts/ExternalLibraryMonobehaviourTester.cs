0f882b694678cb5197d5251b7c9d16ba55928906
AlexJohnKennedy <alexkfridges@gmail.com>
2018-11-08 04:11:51 +1100

    got external libraries to run C# 7 code and .NET 4.7.1 code inside of unity by importing compiled DLLs

--- Assets/Project/Scripts/ExternalLibraryMonobehaviourTester.cs
+++ Assets/Project/Scripts/ExternalLibraryMonobehaviourTester.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityExternalLibraryTEST1_NETv4_6_1;  // IMPORT TEST LIBRARIES, WHICH ARE IN THE PROJECT DIRECTORY AS DLL FILES. 
+using UnityExternalLibraryTEST1_NETv4_7_1;  // IMPORT TEST LIBRARIES, WHICH ARE IN THE PROJECT DIRECTORY AS DLL FILES
 
 public class ExternalLibraryMonobehaviourTester : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
         // Simply try to print the string obtained by the external library
-        Debug.Log(TestLibraryFunctions.GetTestString());
-	}
+        Debug.Log(UnityExternalLibraryTEST1_NETv4_7_1.TestLibraryFunctions.GetTestString());
+        Debug.Log(UnityExternalLibraryTEST1_NETv4_7_1.TestLibraryFunctions.GetTestStringUsingCS7TupleFeature());
+    }
 }
