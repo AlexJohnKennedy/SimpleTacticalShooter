0b883c6fc5848656f7021670c74d537101288590
AlexJohnKennedy <alexkfridges@gmail.com>
2018-12-13 14:07:20 +1100

    fiddled around with some backend api settings in unity and added some compiled external libraries to test compatability differences between .net framewokr and .net standard

--- Assets/Project/Scripts/ExternalLibraryMonobehaviourTester.cs
+++ Assets/Project/Scripts/ExternalLibraryMonobehaviourTester.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityExternalLibraryTEST1_NETv4_7_1;  // IMPORT TEST LIBRARIES, WHICH ARE IN THE PROJECT DIRECTORY AS DLL FILES
+using ClassLibTest_NET_STANDARD_2_0;
 
 public class ExternalLibraryMonobehaviourTester : MonoBehaviour {
 
@@ -10,5 +11,7 @@ public class ExternalLibraryMonobehaviourTester : MonoBehaviour {
         // Simply try to print the string obtained by the external library
         Debug.Log(UnityExternalLibraryTEST1_NETv4_7_1.TestLibraryFunctions.GetTestString());
         Debug.Log(UnityExternalLibraryTEST1_NETv4_7_1.TestLibraryFunctions.GetTestStringUsingCS7TupleFeature());
+        Debug.Log(UnityExternalLibraryTEST1_NETv4_7_1.NetStandard2_WrapperTestLibraryFunctions.PrintDoubleNum(5));
+        Debug.Log(ClassLibTest_NET_STANDARD_2_0.ExternalLibrary_NET_STANDARD_2_0_Test.GetTestStringUsingCS7TupleFeature());
     }
 }
